62cf01de6540b8ea1c737e8a08b575d727716005
Adorabus <adorabusfox@gmail.com>
2024-09-16 18:05:11 -0700

    Change base image from jdk to alpine, fix version finder

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM openjdk:19-jdk-alpine
+FROM alpine:latest
 
 ENV forge_version=recommended
 ENV Xms=6G
 ENV Xmx=6G
 
-RUN apk add --update bash curl
+RUN apk add --update bash curl openjdk21-jre
 
 WORKDIR /usr/local/bin
 COPY forge.sh ./forge
--- forge.sh
+++ forge.sh
@@ -55,18 +55,18 @@ start_server_new () {
 
 if [ -z $forge_version ]
   then
-  echo Environment variable forge_version is required!
+  echo "Environment variable forge_version is required!"
   echo "If you don't know the version, you can use \"latest\" or \"recommended\"."
 elif [ $forge_version = latest ]
   then
-  echo Finding latest version...
-  export forge_version=$(curl -s $FORGE_URL | tr -d "\n" | sed -r 's/(.*Download Latest<br>\s+<small>)([^<]+)(.*)/\2/' | sed 's/ //g')
-  echo Found latest version \"$forge_version\"
+  echo "Finding latest version..."
+  export forge_version=$(curl -s $FORGE_URL | tr -d "\n" | sed -r 's/.*<i class="fa promo-latest".*<small>([^<]+)<\/small>.*/\1/' | sed 's/ //g')
+  echo "Found latest version \"$forge_version\""
 elif [ $forge_version = recommended ]
   then
-  echo Finding recommended version...
-  export forge_version=$(curl -s $FORGE_URL | tr -d "\n" | sed -r 's/(.*Download Recommended<br>\s+<small>)([^<]+)(.*)/\2/' | sed 's/ //g')
-  echo Found recommended version \"$forge_version\"
+  echo "Finding recommended version..."
+  export forge_version=$(curl -s $FORGE_URL | tr -d "\n" | sed -r 's/.*<i class="fa promo-recommended".*<small>([^<]+)<\/small>.*/\1/' | sed 's/ //g')
+  echo "Found recommended version \"$forge_version\""
 fi
 
 if [ -f "forge-$forge_version.jar" ]
